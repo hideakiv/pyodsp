44a41dddb1f96ad07eed69d15807b6594dd5b2f2
hideakiv <h.nakao1992blanca@gmail.com>
2024-12-29 14:05:24 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup 
+  
+setup( 
+    name='pyodec', 
+    version='0.1', 
+    description='Decomposition Algorithms for Block structured problems in Pyomo', 
+    author='Hideaki Nakao', 
+    author_email='1@gmail.com', 
+    packages=['pyodec'], 
+    install_requires=[ 
+        'numpy', 
+        'Pyomo',
+    ], 
+) 

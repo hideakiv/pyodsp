26de4145ea1980cf01589ed1c095262b5f04702e
hideakiv <h.nakao1992blanca@gmail.com>
2025-02-04 22:51:24 +0900

    created solver class

---
+++ pyodec/solver/pyomo_solver.py
@@ -0,0 +1,104 @@
+from typing import List
+from pyomo.environ import (
+    ConcreteModel,
+    SolverFactory,
+    Objective,
+    value,
+)
+from pyomo.core.base.var import VarData
+from pyomo.opt import TerminationCondition
+
+from .solver import Solver
+
+solver_dual_sign_convention = dict()
+solver_dual_sign_convention["ipopt"] = -1
+solver_dual_sign_convention["gurobi"] = -1
+solver_dual_sign_convention["gurobi_direct"] = -1
+solver_dual_sign_convention["gurobi_persistent"] = -1
+solver_dual_sign_convention["cplex"] = -1
+solver_dual_sign_convention["cplex_direct"] = -1
+solver_dual_sign_convention["cplexdirect"] = -1
+solver_dual_sign_convention["cplex_persistent"] = -1
+solver_dual_sign_convention["glpk"] = -1
+solver_dual_sign_convention["cbc"] = -1
+solver_dual_sign_convention["xpress_direct"] = -1
+solver_dual_sign_convention["xpress_persistent"] = -1
+solver_dual_sign_convention["appsi_highs"] = 1
+
+
+class PyomoSolver(Solver):
+    """Base class for solvers using Pyomo"""
+
+    def __init__(
+        self, model: ConcreteModel, solver: str, vars: List[VarData], **kwargs
+    ):
+        """Initialize the subsolver.
+
+        Args:
+            model: The Pyomo model.
+            solver: The solver to use.
+        """
+        self.solver = SolverFactory(solver)
+        self.model = model
+        self.vars = vars
+        self._solver_kwargs = kwargs
+
+        self.original_objective = self._get_objective()
+
+        self._results = None
+
+    def solve(self) -> None:
+        """Solve the model."""
+
+        self._results = self.solver.solve(
+            self.model, load_solutions=False, **self._solver_kwargs
+        )
+        if self.is_optimal():
+            self.model.solutions.load_from(self._results)
+
+    def _get_objective(self) -> Objective:
+        """Get the objective of the model"""
+        for obj in self.model.component_objects(Objective, active=True):
+            # There should be only one objective
+            return obj
+        else:
+            raise ValueError("Objective not found")
+
+    def get_objective_value(self) -> float:
+        """Get the objective value of the model"""
+        return value(self.get_objective())
+
+    def get_objective_sense(self) -> bool:
+        """Get the sense of the objective.
+
+        Returns:
+            True if the objective is to be minimized, False otherwise.
+        """
+        return self.get_objective().sense > 0
+
+    def get_solution(self) -> List[float]:
+        """Get the solution of the model.
+
+        Args:
+            vars: The variables to get the solution of.
+        """
+        return [var.value for var in self.vars]
+
+    def is_optimal(self) -> bool:
+        """Returns whether the model is optimal."""
+        return (
+            self._results.solver.termination_condition == TerminationCondition.optimal
+        )
+
+    def is_infeasible(self) -> bool:
+        """Returns whether the model is infeasible."""
+        return (
+            self._results.solver.termination_condition
+            == TerminationCondition.infeasible
+        )
+
+    def is_unbounded(self) -> bool:
+        """Returns whether the model is unbounded."""
+        return (
+            self._results.solver.termination_condition == TerminationCondition.unbounded
+        )
---
+++ pyodec/solver/solver.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Solver(ABC):
+    """Base class for Dual Decomposition subproblem solvers"""
+
+    @abstractmethod
+    def solve(self) -> None:
+        """Solve the model."""
+        pass
+
+    @abstractmethod
+    def get_objective_value(self) -> float:
+        """Get the objective value of the model"""
+        return 0.0
+
+    @abstractmethod
+    def get_objective_sense(self) -> bool:
+        """Get the sense of the objective.
+
+        Returns:
+            True if the objective is to be minimized, False otherwise.
+        """
+        return True
+
+    @abstractmethod
+    def get_solution(self) -> List[float]:
+        """Get the solution of the model."""
+        return []
+
+    @abstractmethod
+    def is_optimal(self) -> bool:
+        """Returns whether the model is optimal."""
+        return False
+
+    @abstractmethod
+    def is_infeasible(self) -> bool:
+        """Returns whether the model is infeasible."""
+        return False
+
+    @abstractmethod
+    def is_unbounded(self) -> bool:
+        """Returns whether the model is unbounded."""
+        return False

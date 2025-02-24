a788e98fdb6758f58ec129e51fc3aea569567d32
hideakiv <h.nakao1992blanca@gmail.com>
2025-02-24 14:36:18 +0900

    refactored dd run and run_mpi

--- pyodsp/dec/dd/run_mpi.py
+++ pyodsp/dec/dd/run_mpi.py
@@ -6,6 +6,7 @@ from .run import DdRun
 from .node import DdNode
 from .node_leaf import DdLeafNode
 from .node_root import DdRootNode
+from ..utils import SparseMatrix
 
 
 class DdRunMpi(DdRun):
@@ -18,104 +19,118 @@ class DdRunMpi(DdRun):
         self.rank = self.comm.Get_rank()
 
     def run(self):
-        matrix_info: Dict[int, List[Dict[int, float]]] = {}
         if self.rank == 0:
-            self.logger.log_initialization()
-            root = self.nodes[self.root_idx]
-            assert isinstance(root, DdRootNode)
-            root.build()
-            matrices: Dict[int, Dict[int, List[Dict[int, float]]]] = {}
-            for child_id in root.get_children():
-                target = self.node_rank_map[child_id]
-                if target not in matrices:
-                    matrices[target] = {}
-                matrices[target][child_id] = root.alg.lagrangian_data.matrix[child_id]
+            self._init_root()
+            matrices = self._split_matrices()
 
             for target, matrix in matrices.items():
                 self.comm.send(matrix, dest=target, tag=0)
 
             if 0 in matrices:
                 matrix_info = matrices[0]
-                for node in self.nodes.values():
-                    if isinstance(node, DdLeafNode):
-                        node_id = node.idx
-                        node.set_coupling_matrix(matrix_info[node_id])
+                for node_id, matrix in matrix_info.items():
+                    self._init_leaf(node_id, matrix)
 
-            root.alg.reset_iteration()
-            solution = [0.0 for _ in range(root.num_constrs)]
+            self._run_root()
+            self.logger.log_finaliziation()
+
+            self._finalize_root()
+        else:
+            matrix_info = self.comm.recv(source=0, tag=0)
+
+            for node_id, matrix in matrix_info.items():
+                self._init_leaf(node_id, matrix)
+
+            self._run_leaf_mpi()
+
+            self._finalize_leaf_mpi()
+
+        for node in self.nodes.values():
+            node.save(self.filedir)
+
+    def _split_matrices(self) -> Dict[int, Dict[int, SparseMatrix]]:
+        matrices: Dict[int, Dict[int, SparseMatrix]] = {}
+        for child_id in self.root.get_children():
+            target = self.node_rank_map[child_id]
+            if target not in matrices:
+                matrices[target] = {}
+            matrices[target][child_id] = self.root.alg.lagrangian_data.matrix[child_id]
+        return matrices
+    
+    def _run_root(self) -> None:
+        self.root.alg.reset_iteration()
+        solution = [0.0 for _ in range(self.root.num_constrs)]
+        
+        # broadcast solution
+        self.comm.bcast(solution, root=0)
+
+        cuts_dn = self._run_leaf(solution)
+
+        # gather cuts
+        all_cuts_dn = self.comm.gather(cuts_dn, root=0)
+        combined_cuts_dn = {}
+        for d in all_cuts_dn:
+            combined_cuts_dn.update(d)
+
+        while True:
+            solution = self.root.run_step(combined_cuts_dn)
+            if solution is None:
+                self.comm.bcast(-1, root=0)
+                break
+            # broadcast solution
             self.comm.bcast(solution, root=0)
+
             cuts_dn = self._run_leaf(solution)
+            
+            # gather cuts
             all_cuts_dn = self.comm.gather(cuts_dn, root=0)
             combined_cuts_dn = {}
             for d in all_cuts_dn:
                 combined_cuts_dn.update(d)
-            while True:
-                solution = root.run_step(combined_cuts_dn)
-                if solution is None:
-                    self.comm.bcast(-1, root=0)
-                    break
-                self.comm.bcast(solution, root=0)
 
-                cuts_dn = self._run_leaf(solution)
-                all_cuts_dn = self.comm.gather(cuts_dn, root=0)
-                combined_cuts_dn = {}
-                for d in all_cuts_dn:
-                    combined_cuts_dn.update(d)
-        else:
-            matrix_info = self.comm.recv(source=0, tag=0)
-
-            for node in self.nodes.values():
-                if isinstance(node, DdLeafNode):
-                    node_id = node.idx
-                    node.set_coupling_matrix(matrix_info[node_id])
-
-            solution: List[float] = None
+    def _run_leaf_mpi(self) -> None:
+        solution: List[float] = None
+        solution = self.comm.bcast(solution, root=0)
+        cuts_dn = self._run_leaf(solution)
+        all_cuts_dn = self.comm.gather(cuts_dn, root=0)
+        while True:
             solution = self.comm.bcast(solution, root=0)
+            if solution == -1:
+                break
             cuts_dn = self._run_leaf(solution)
             all_cuts_dn = self.comm.gather(cuts_dn, root=0)
-            while True:
-                solution = self.comm.bcast(solution, root=0)
-                if solution == -1:
-                    break
-                cuts_dn = self._run_leaf(solution)
-                all_cuts_dn = self.comm.gather(cuts_dn, root=0)
 
-        if self.rank == 0:
-            self.logger.log_finaliziation()
-            root = self.nodes[self.root_idx]
-            assert isinstance(root, DdRootNode)
+    def _finalize_root(self) -> None:
+        solutions = self.root.solve_mip_heuristic()
 
-            solutions = root.solve_mip_heuristic()
-            solutions_dict: Dict[int, Dict[int, List[float]]] = {}
-            for child_id in root.get_children():
-                target = self.node_rank_map[child_id]
-                if target not in solutions_dict:
-                    solutions_dict[target] = {}
-                solutions_dict[target][child_id] = solutions[child_id]
+        # split solutions
+        solutions_dict: Dict[int, Dict[int, List[float]]] = {}
+        for child_id in self.root.get_children():
+            target = self.node_rank_map[child_id]
+            if target not in solutions_dict:
+                solutions_dict[target] = {}
+            solutions_dict[target][child_id] = solutions[child_id]
 
-            for target, sols in solutions_dict.items():
-                self.comm.send(sols, dest=target, tag=1)
-            
-            final_obj = 0.0
-            if 0 in solutions_dict:
-                for node in self.nodes.values():
-                    if isinstance(node, DdLeafNode):
-                        node.alg.fix_variables_and_solve(solutions[node.idx])
-                    final_obj += node.alg.get_objective_value()
-            all_objs = self.comm.gather(final_obj, root=0)
-            total_obj = 0.0
-            for objval in all_objs:
-                total_obj += objval
-            self.logger.log_completion(total_obj)
-        else:
-            solutions_info = self.comm.recv(source=0, tag=1)
-            final_obj = 0.0
+        for target, sols in solutions_dict.items():
+            self.comm.send(sols, dest=target, tag=1)
+        
+        final_obj = 0.0
+        if 0 in solutions_dict:
             for node in self.nodes.values():
                 if isinstance(node, DdLeafNode):
-                    node.alg.fix_variables_and_solve(solutions_info[node.idx])
-                    final_obj += node.alg.get_objective_value()
-            all_objs = self.comm.gather(final_obj, root=0)
-            
+                    node.alg.fix_variables_and_solve(solutions[node.idx])
+                final_obj += node.alg.get_objective_value()
+        all_objs = self.comm.gather(final_obj, root=0)
+        total_obj = 0.0
+        for objval in all_objs:
+            total_obj += objval
+        self.logger.log_completion(total_obj)
 
+    def _finalize_leaf_mpi(self):
+        solutions_info = self.comm.recv(source=0, tag=1)
+        final_obj = 0.0
         for node in self.nodes.values():
-            node.save(self.filedir)
+            if isinstance(node, DdLeafNode):
+                node.alg.fix_variables_and_solve(solutions_info[node.idx])
+                final_obj += node.alg.get_objective_value()
+        all_objs = self.comm.gather(final_obj, root=0)

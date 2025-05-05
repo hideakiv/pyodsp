3722869f7890e4ac69a0834c8208512d92341e20
hideakiv <h.nakao1992blanca@gmail.com>
2025-05-05 10:03:30 +0900

    deleted methods in DdAlgLeaf

--- pyodsp/dec/dd/alg_leaf.py
+++ pyodsp/dec/dd/alg_leaf.py
@@ -1,22 +1,7 @@
-from abc import ABC, abstractmethod
-from typing import List, Dict, Tuple
+from abc import ABC
 
 from ..node._alg import IAlgLeaf
 
 
 class DdAlgLeaf(IAlgLeaf, ABC):
-    @abstractmethod
-    def set_coupling_matrix(self, coupling_matrix: List[Dict[int, float]]) -> None:
-        pass
-
-    @abstractmethod
-    def get_solution_or_ray(self) -> Tuple[bool, List[float], float]:
-        pass
-
-    @abstractmethod
-    def get_len_vars(self) -> int:
-        pass
-
-    @abstractmethod
-    def fix_variables_and_solve(self, values: List[float]) -> None:
-        pass
+    pass

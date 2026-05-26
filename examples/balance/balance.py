d897dd4f671c179fd45581cc71a40200150d2177
hideakiv <h.nakao1992blanca@gmail.com>
2026-05-26 09:12:46 +0900

    update model

--- examples/balance/balance.py
+++ examples/balance/balance.py
@@ -26,72 +26,42 @@ class Schedule:
     max_proc: list[float]
 
 
-def root_stage(
+def stage(
     block: pyo.ScalarBlock,
-    prev_level: float,
-    schedule: Schedule,
-):
-    block.prev_level = pyo.Var(domain=pyo.NonNegativeReals)
-    block.prev_level.fix(prev_level)
-
-    # define next procurement
-    def proc_bd(block, t):
-        return (0, schedule.max_proc[t])
-
-    block.next_procurement = pyo.Var(
-        block.nextT, domain=pyo.NonNegativeReals, bounds=proc_bd
-    )
-
-    # define cost
-    objexpr = sum(
-        schedule.proc_prices[t] * block.next_procurement[t]
-        for t in range(schedule.next_time)
-    )
-
-    block.obj = pyo.Objective(expr=objexpr, sense=pyo.minimize)
-
-
-def nonroot_stage(
-    block: pyo.ScalarBlock,
-    prev_level: pyo.Var,
-    procurement: pyo.Var,
+    current_level: pyo.Var,
     schedule: Schedule,
     spec: Spec,
 ):
-    block.T = pyo.RangeSet(0, schedule.time - 1)
+
     block.nextT = pyo.RangeSet(0, schedule.next_time - 1)
 
-    # define power
-    block.power = pyo.Var(block.T, domain=pyo.Reals)
+    # define hydro
 
     def discharge_bd(block, t):
         return (0, spec.max_discharge)
 
     block.power_discharge = pyo.Var(
-        block.T, domain=pyo.NonNegativeReals, bounds=discharge_bd
+        block.nextT, domain=pyo.NonNegativeReals, bounds=discharge_bd
     )
 
     def charge_bd(block, t):
         return (0, spec.max_charge)
 
-    block.power_charge = pyo.Var(block.T, domain=pyo.NonNegativeReals, bounds=charge_bd)
-
-    def power_rule(block, t):
-        return block.power[t] == block.power_discharge[t] - block.power_charge[t]
-
-    block.power_cn = pyo.Constraint(block.T, rule=power_rule)
+    block.power_charge = pyo.Var(
+        block.nextT, domain=pyo.NonNegativeReals, bounds=charge_bd
+    )
 
     # define level
     def level_bd(block, t):
         return (spec.min_level, spec.max_level)
 
-    block.level = pyo.Var(block.T, domain=pyo.NonNegativeReals, bounds=level_bd)
+    block.level = pyo.Var(block.nextT, domain=pyo.NonNegativeReals, bounds=level_bd)
 
     def level_rule(block, t):
         if t == 0:
             return (
                 block.level[t]
-                == prev_level
+                == current_level
                 - block.power_discharge[t]
                 + spec.charge_efficiency * block.power_charge[t]
             )
@@ -103,7 +73,56 @@ def nonroot_stage(
                 + spec.charge_efficiency * block.power_charge[t]
             )
 
-    block.level_cn = pyo.Constraint(block.T, rule=level_rule)
+    block.level_cn = pyo.Constraint(block.nextT, rule=level_rule)
+
+    # define procurement
+    def proc_bd(block, t):
+        return (0, schedule.max_proc[t])
+
+    block.procurement = pyo.Var(
+        block.nextT, domain=pyo.NonNegativeReals, bounds=proc_bd
+    )
+
+    # define power
+    block.power = pyo.Var(block.nextT, domain=pyo.Reals)
+
+    def power_rule(block, t):
+        return (
+            block.power[t]
+            == block.procurement[t] + block.power_discharge[t] - block.power_charge[t]
+        )
+
+    block.power_cn = pyo.Constraint(block.nextT, rule=power_rule)
+
+
+def root_stage(
+    block: pyo.ScalarBlock,
+    current_level: float,
+    schedule: Schedule,
+    spec: Spec,
+):
+
+    block.current_level = pyo.Var(domain=pyo.NonNegativeReals)
+    block.current_level.fix(current_level)
+    stage(block, block.current_level, schedule, spec)
+
+    # define cost
+    objexpr = sum(
+        schedule.proc_prices[t] * block.procurement[t]
+        for t in range(schedule.next_time)
+    )
+
+    block.obj = pyo.Objective(expr=objexpr, sense=pyo.minimize)
+
+
+def nonroot_stage(
+    block: pyo.ScalarBlock,
+    current_level: pyo.Var,
+    power: pyo.Var,
+    schedule: Schedule,
+    spec: Spec,
+):
+    block.T = pyo.RangeSet(0, schedule.time - 1)
 
     # define balance
     def market_bd(block, t):
@@ -116,23 +135,13 @@ def nonroot_stage(
 
     def balance_rule(block, t):
         return (
-            block.power[t]
-            + block.market[t]
-            + procurement[t]
-            + block.violation_p[t]
-            - block.violation_m[t]
+            block.market[t] + power[t] + block.violation_p[t] - block.violation_m[t]
             == schedule.demands[t]
         )
 
     block.balance_cn = pyo.Constraint(block.T, rule=balance_rule)
 
-    # define next procurement
-    def proc_bd(block, t):
-        return (0, schedule.max_proc[t])
-
-    block.next_procurement = pyo.Var(
-        block.nextT, domain=pyo.NonNegativeReals, bounds=proc_bd
-    )
+    stage(block, current_level, schedule, spec)
 
     # define cost
     proc_cost = sum(
